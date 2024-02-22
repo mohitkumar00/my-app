5ec604318c1a9bc78fced396534c9c4a12e5e2dd
Mohit Kumar <mohit.kumar@planview.com>
2024-02-22 21:50:29 +0530

    Added dark mode toggle

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const[mode, setMode] = useState('light')
+
+  const toggleMode = ()=>{
+    console.log(mode)
+    if(mode === 'light'){
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }
   return (
     <>
-    <Navbar title="TextUtilityTool" myAboutText="About TextUtilsApp"/>
+    <Navbar title="TextUtilityTool" myAboutText="About TextUtilsApp" mode={mode} toggleMode={toggleMode}/>
     <div className='textAreaContainer my-3' >
     <TextForm heading="Enter your Text to Analyze" text="Text"/>
     </div>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,25 +5,67 @@ export default function TextForm(props) {
             setText(event.target.value)
     }
     const UpperCaseHandler = (event)=>{
-        console.log(text)
         let newText = text.toUpperCase();
-        setText(newText)
+        setText(newText);
         
     }
-    const[text, setText] = useState("")
+    const LowerCaseHandler = (event)=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+        
+    }
+    const OnClearHandler = ()=>{
+        setText("");
+    }
+    const[text, setText] = useState("");
+    const[btnText, setBtnText] = useState("Enable Dark Mode");
+    const [styleState, setStyeleStet] = useState({
+        color:'black',
+        backgroundColor:'white'
+    })
+    const OnViewModeHandler = ()=>{
+        if(btnText === 'Enable Dark Mode')
+        {
+            setStyeleStet({
+                color: 'white',
+                backgroundColor: 'black'
+            })
+            setBtnText("Enable Light Mode");
+        }
+        else{
+            setStyeleStet({
+                color: 'black',
+                backgroundColor: 'white'
+            })
+            setBtnText("Enable Dark Mode")
+        }
+    }
   return (
+    <>
       <div className="form-floating">
         <h1>{props.heading}</h1>
         <div className ="mb-3">
         <textarea rows="8"
-          className="form-control my-3"
+          className="form-control my-3" style={styleState}
           placeholder={text}
           id="floatingTextarea"
           value={text}
           onChange={textAreaOnchangeHandler}
         ></textarea>
         <button type="button" className="btn btn-primary" onClick={UpperCaseHandler}>Convert to UpperCase</button>
+        <button type="button" className="btn btn-primary mx-3" onClick={LowerCaseHandler}>Convert to LowerCase</button>
+        <button type="button" className="btn btn-primary mx-3" onClick={OnClearHandler}>Clear</button>
+        <button type="button" className="btn btn-primary mx-3" onClick={OnViewModeHandler}>{btnText}</button>
         </div>
       </div>
+      <div className="Container my-3">
+        <h1>Your Text Summary</h1>
+        <p>{text.split(" ").length-1} words and {text.length} characters</p>
+      </div>
+      <div className="Container  my-2">
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+      </>
   );
 }
